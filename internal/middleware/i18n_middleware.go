69494c1b11d1ed993c7965d7a8f34626dd8f3f38
uooobarry <383435763@qq.com>
2025-04-22 16:08:29 +0800

    tweak: lang query first

--- internal/middleware/i18n_middleware.go
+++ internal/middleware/i18n_middleware.go
@@ -8,10 +8,10 @@ import (
 
 func I18nMildware(i18n *i18n.I18nHelper) gin.HandlerFunc {
 	return func(c *gin.Context) {
-		if lng := c.Query("lang"); lng != "" {
+		if lng := c.GetHeader("Accept-Language"); lng != "" {
 			c.Set("lang", lng)
 		}
-		if lng := c.GetHeader("Accept-Language"); lng != "" {
+		if lng := c.Query("lang"); lng != "" {
 			c.Set("lang", lng)
 		}
 
